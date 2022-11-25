2688c2c3d84dedbefcc0e2157699d55af5ac1a70
Selaka Nanayakkara <selaka.nanayakkara@cube.global>
2022-11-25 21:29:20 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace puppeteer_tor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
