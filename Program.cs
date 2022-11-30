b61f4a7221100002f10d30ae11962eed40eccd62
Selaka Nanayakkara <selaka.nanayakkara@cube.global>
2022-11-30 16:12:39 +0530

    Firefox content fetching completed.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 using PuppeteerSharp;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,35 +14,47 @@ namespace puppeteer_tor
         static async Task Main(string[] args)
         {
 
-            string enableAutomation = "--enable-automation";
-            string noSandBox = "--no-sandbox";
-            string disableSetUidSandBox = "--disable-setuid-sandbox";
-            string[] argumentsWithoutExtension = new string[] { 
-                "C:\\Users\\selaka.nanayakkara\\Desktop\\Tor Browser\\Browser\\TorBrowser\\Data\\profile.default",
-                "--proxy-server=socks5://127.0.0.1:9050",
-                "--disable-gpu",
-                "--disable-dev-shm-usage",
-                enableAutomation, disableSetUidSandBox, noSandBox };
 
-            var options = new LaunchOptions
+            // Initiating Browser configuration
+            Console.WriteLine("Intiating Tor Browser");
+ 
+             Browser browser = (Browser)await Puppeteer.LaunchAsync(new LaunchOptions
             {
                 Headless = false,
                 ExecutablePath = @"C:\Users\selaka.nanayakkara\Desktop\Tor Browser\Browser\firefox.exe",
-                Args = argumentsWithoutExtension
-            };
+                Product = Product.Firefox,
+                UserDataDir = @"C:\Users\selaka.nanayakkara\Desktop\Tor Browser\Browser\TorBrowser\Data\profile.default",
+                DefaultViewport = null,
+                IgnoreHTTPSErrors = true,
+                Args = new[] { "-wait-for-browser" }
+            });
 
-            using (var browser = await Puppeteer.LaunchAsync(options))
-            {
-                Thread.Sleep(5000);
-                var page = await browser.NewPageAsync();
-                await page.GoToAsync("https://check.torproject.org/");
-                var element = await page.WaitForSelectorAsync("h1");
-                var text = element.ToString();
+            // Enabling prxoy connectivilty
+            Console.WriteLine("Intiating Tor proxy");
+            var page = await browser.PagesAsync();
+            Page page1 =(Page)page[0];
+            await page1.ClickAsync("#connectButton");
 
-            }
-        }
+            // Loading geoblocked url.
+            Console.WriteLine("Navigating to the URL");
+            Page page3 =(Page)await browser.NewPageAsync();
+            page3.DefaultNavigationTimeout = 0;
+            await page3.GoToAsync("http://nebraskalegislature.gov/laws/browse-chapters.php?chapter=20");
 
+            // Fetching content from the page.
+            Console.WriteLine("Fetching content in the URL.");
+            var content = await page3.GetContentAsync();
+            
+            Console.WriteLine("Content fetching completed! ");
+
+            // Closing Browser
+            Console.WriteLine("Closing browser.");
+            await browser.CloseAsync();
+
+         }
     }
 
-}
+ }
+
+
 
